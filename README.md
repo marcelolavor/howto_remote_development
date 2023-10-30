067ccf693f3dc5827d3e9dc67c27af171a9edd15
Marcelo Lavor <ex102258@edp.pt>
2023-10-30 23:39:14 +0000

    Content adds

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # howto_sshkeys
 How to deal with ssh keys forever .....kkkk
+
+## Config your machine
+1 - Config your Windows machine (powershell_config.ps1)
+
+2 - Create a SSH Key (ssh_config.ps1)
+
+## Config applications accesses:
+
+- GitHub (github_conn.ps1)
+- Bitbucket (bitbucket_conn.ps1)
+- SourceTree (sourcetree_config.ps1)
+- Virutal Machine SSH (vm_conn.ps1)
---
+++ sourcetree ssh service/sourcetree_config.ps1
@@ -0,0 +1,6 @@
+# SourceTree Config
+
+# 1 - SourceTree Config only accepts PuTTY SSH Key formats, so you have to convert any ssh key that is not in .ppk file format
+# 2 - Go to "Tools > Create or Import SSH Keys > Load " and select all files option in the pop up windows and select the source file with your ssh private key and OK
+# 3 - After the file be loaded and the Key be properly imported, select "Save Private Key", define a new file name with ".ppk" extention at the end
+# 4 - Come back to the SourceTree interface and go to "Tools > Launch SSH Agent", select the new file with ".ppk" extention at the end and OK.
