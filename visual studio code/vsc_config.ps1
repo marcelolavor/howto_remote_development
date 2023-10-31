bad8c0de161872314fbc1270ebcaef3a65ee4330
Marcelo Lavor <ex102258@edp.pt>
2023-10-31 00:32:28 +0000

    VSC config information

---
+++ visual studio code/vsc_config.ps1
@@ -0,0 +1,21 @@
+# *************************
+# Install Extentions
+# *************************
+
+# In VS Code, open the Command Palette (F1) and type "Remote-SSH: Open Configuration File"
+# Selec "REMOTE: Install Remote Development Extensions"
+# On Extentions: MARKETPLACE select some suggested extentions
+# -  WSL
+# - Dev Containers
+# - Remote - SSH
+
+
+# *************************
+# Config some extentions
+# *************************
+
+# Remote - SSH
+# https://code.visualstudio.com/docs/remote/troubleshooting
+
+
+
---
+++ wsl service/wsl_config.ps1
@@ -0,0 +1,10 @@
+# Install WSL (https://learn.microsoft.com/pt-br/windows/wsl/install)
+
+# 1 - List and choose Linux desirable release 
+# wsl.exe -l -o.
+
+# 1 - Open PowerShell windows as Admin and run "wsl --install -d Ubuntu"
+
+Ubuntu-22.04
+# Reference:
+# https://code.visualstudio.com/docs/remote/wsl#_advanced-environment-setup-script
