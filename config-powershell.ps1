5e1f91874268225f81b09e0f6a8ca7c364de404a
Marcelo Lavor <ex102258@edp.pt>
2023-11-05 23:43:28 +0000

    Powershell additions

--- config-powershell.ps1
+++ config-powershell.ps1
@@ -11,6 +11,12 @@ winget install --id Microsoft.Powershell --source winget
 # Install PowerShell Preview
 winget install --id Microsoft.Powershell.Preview --source winget
 
+# Atualizar as variáves de ambiente numa sessão PS
+$env:PATH = [System.Environment]::GetEnvironmentVariable('PATH', 'User') + ";" + [System.Environment]::GetEnvironmentVariable('PATH', 'Machine') 
+
+# Listar os valores do PATH
+[System.Environment]::GetEnvironmentVariable('PATH', 'User') + ";" + [System.Environment]::GetEnvironmentVariable('PATH', 'Machine') -split ";"
+
 
 Reference:
 https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.3#install-powershell-using-winget-recommended
