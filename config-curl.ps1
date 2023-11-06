7fecc9b754845afc70057e4e407bba0bede3370d
Marcelo Taveira Lavor <Marcelo.Lavor@gmail.com>
2023-11-06 00:12:48 +0000

    Updated cURL env variables addition

--- config-curl.ps1
+++ config-curl.ps1
@@ -5,3 +5,16 @@ winget search cURL.cURL
 
 ## Instala a última versão
 winget install --id=cURL.cURL  -e
+
+# ADICIONAR REFERÊNCIAS AO CURL PARA EXECUÇÂO
+$curl_home = "C:\Users\<your-user>\AppData\Local\Microsoft\WinGet\Packages\<curl-bin-path>"
+
+# LOCAL USER
+[Environment]::SetEnvironmentVariable("CURL_HOME", $curl_home, "User") # Adicionar variável de ambiente CURL_HOME
+[Environment]::SetEnvironmentVariable("PATH", $curl_home + ";" + [Environment]::GetEnvironmentVariable("PATH", "User") , "User") # Adicionar o cURL ao PATH
+[Environment]::GetEnvironmentVariable("PATH", "User") -split ";" # Verificar adição
+
+# LOCAL MACHINE (OPCIONAL)
+[Environment]::SetEnvironmentVariable("CURL_HOME", $curl_home, "Machine") # Adicionar variável de ambiente CURL_HOME
+[Environment]::SetEnvironmentVariable("PATH", $curl_home + ";" + [Environment]::GetEnvironmentVariable("PATH", "Machine") , "Machine") # Adicionar o cURL ao PATH
+[Environment]::GetEnvironmentVariable("PATH", "Machine") -split ";" # Verificar adição
